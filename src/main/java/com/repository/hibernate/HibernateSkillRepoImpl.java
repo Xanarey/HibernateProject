99f9df4b967f0d8d65b567ae9728695d9e35884b
Xanarey <doctor.layout@mail.ru>
2022-10-01 13:34:04 +0300

    complete

--- src/main/java/com/repository/hibernate/HibernateSkillRepoImpl.java
+++ src/main/java/com/repository/hibernate/HibernateSkillRepoImpl.java
@@ -4,39 +4,55 @@ import com.model.Skill;
 import com.repository.SkillRepo;
 import com.utils.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.Collections;
 import java.util.List;
 
 public class HibernateSkillRepoImpl implements SkillRepo {
 
     @Override
     public List<Skill> getAll() {
-        return HibernateUtil.getSession().createQuery("FROM Skill", Skill.class).list();
-
+        try (Session session = HibernateUtil.getSession()){
+            return session.createQuery("FROM Skill", Skill.class).list();
+        } catch (Throwable t) {
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public Skill getById(Long id) {
-        return HibernateUtil.getSession().get(Skill.class, id);
+        try (Session session = HibernateUtil.getSession()) {
+            return session.get(Skill.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Skill();
     }
 
     @Override
     public Skill insert(Skill skill) {
-        Session session = HibernateUtil.getSession();
-        session.beginTransaction();
-        session.merge(skill);
-        session.getTransaction().commit();
-        session.close();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()){
+            transaction = session.beginTransaction();
+            session.merge(skill);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+        }
         return skill;
     }
 
     @Override
     public void deleteById(Long id) {
-        Session session = HibernateUtil.getSession();
-        session.beginTransaction();
-        session.remove(session.get(Skill.class, id));
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = HibernateUtil.getSession()){
+            session.beginTransaction();
+            session.remove(session.get(Skill.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/main/java/com/repository/hibernate/HibernateSpecRepoImpl.java
+++ src/main/java/com/repository/hibernate/HibernateSpecRepoImpl.java
@@ -4,38 +4,56 @@ import com.model.Specialty;
 import com.repository.SpecialtyRepo;
 import com.utils.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.Collections;
 import java.util.List;
 
 public class HibernateSpecRepoImpl implements SpecialtyRepo  {
 
     @Override
     public List<Specialty> getAll() {
-        return HibernateUtil.getSession().createQuery("FROM Specialty", Specialty.class).list();
+        try (Session session = HibernateUtil.getSession()){
+            return session.createQuery("FROM Specialty", Specialty.class).list();
+        } catch (Throwable t) {
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public Specialty getById(Long id) {
-        return HibernateUtil.getSession().get(Specialty.class, id);
+        try (Session session = HibernateUtil.getSession()){
+            return session.get(Specialty.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Specialty();
     }
 
     @Override
     public Specialty insert(Specialty specialty) {
-        Session session = HibernateUtil.getSession();
-        session.beginTransaction();
-        session.merge(specialty);
-        session.getTransaction().commit();
-        session.close();
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSession()) {
+            transaction = session.beginTransaction();
+            session.merge(specialty);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
         return specialty;
     }
 
     @Override
     public void deleteById(Long id) {
-        Session session = HibernateUtil.getSession();
-        session.beginTransaction();
-        session.remove(session.get(Specialty.class, id));
-        session.getTransaction().commit();
-        session.close();
+        try (Session session = HibernateUtil.getSession()){
+            session.beginTransaction();
+            session.remove(session.get(Specialty.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
